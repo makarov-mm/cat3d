44ed30cd08494aaa0256cf43664f192cf545492c
Mykhailo Makarov <m.m.makarov@gmail.com>
2026-04-15 14:16:35 +0200

    Fix files paths

--- Files.cs
+++ Files.cs
@@ -2,9 +2,9 @@
 
 public static class Files
 {
-    public static string Model => "..\\..\\..\\..\\objects\\12221_Cat_v1_l3.obj";
-    public static string TextureDiffuse => "..\\..\\..\\..\\objects\\Cat_diffuse.jpg";
-    public static string TextureBump => "..\\..\\..\\..\\objects\\Cat_bump.jpg";
-    public static string ShaderVertex => "..\\..\\..\\shaders\\cat.vert";
-    public static string ShaderFragment => "..\\..\\..\\shaders\\cat.frag";
+    public static string Model => Path.Combine(AppContext.BaseDirectory, "objects", "12221_Cat_v1_l3.obj");
+    public static string TextureDiffuse => Path.Combine(AppContext.BaseDirectory, "objects", "Cat_diffuse.jpg");
+    public static string TextureBump => Path.Combine(AppContext.BaseDirectory, "objects", "Cat_bump.jpg");
+    public static string ShaderVertex => Path.Combine(AppContext.BaseDirectory, "shaders", "cat.vert");
+    public static string ShaderFragment => Path.Combine(AppContext.BaseDirectory, "shaders", "cat.frag");
 }
